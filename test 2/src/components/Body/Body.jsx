46fdbd949d83faa077a6020e95921c0a312ecabe
sophienunez <98788221+sophienunez@users.noreply.github.com>
2024-02-01 19:06:37 -0500

    Add files via upload

---
+++ test 2/src/components/Body/Body.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+export function Body (){
+    return(
+        <main>
+            <h2>this is actually a page ☝🏻🤓</h2>
+            <p>in this page I'm trying to do som stuff but i dunno if this will work bye bye</p>
+        </main>
+    )
+}
---
+++ test 2/src/components/Footer/Footer.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+export function Footer (){
+    return(
+        <footer>
+            <h2>this is the footer of the page</h2>
+        </footer>
+    )
+}
